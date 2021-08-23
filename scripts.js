6c3f25f2c5105300adc09660460c48d1da4dd042
Craig <84283410+cshields1@users.noreply.github.com>
2021-08-22 21:41:48 -0700

    Target new display div ID

--- scripts.js
+++ scripts.js
@@ -17,12 +17,33 @@ function addBookToLibrary() {
 }
 
 function displayBooks() {
-  const display = document.querySelector("div");
+  const display = document.querySelector("#display");
 
   myLibrary.forEach((book) => {
-    const libraryBook = document.createElement("div");
-    libraryBook.classList.add("card");
-    libraryBook.textContent = book.title;
-    display.appendChild(libraryBook);
+    const card = document.createElement("div");
+    card.classList.add("card", "mx-auto", "my-3");
+
+    const cardHeader = document.createElement("h5");
+    cardHeader.classList.add("card-header");
+    cardHeader.textContent = book.title;
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    const bookAuthor = document.createElement("h6");
+    bookAuthor.classList.add("card-subtitle");
+    bookAuthor.textContent = `${book.author} (author)`;
+
+    const bookInfo = document.createElement("p");
+    bookInfo.classList.add("card-text");
+    bookInfo.textContent = `${book.pages} pages long; ${
+      book.hasBeenRead ? "has been read" : "has not been read"
+    }.`;
+
+    cardBody.append(bookAuthor, bookInfo);
+
+    card.append(cardHeader, cardBody);
+
+    display.append(card);
   });
 }
